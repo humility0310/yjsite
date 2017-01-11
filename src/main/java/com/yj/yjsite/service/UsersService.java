1b5c365d04eae1e345c602b58f41a5ca4b158b78
kyj <BIT@DESKTOP-DOGVD1T>
2017-01-11 14:05:47 +0900

    ori code

--- src/main/java/com/yj/yjsite/service/UsersService.java
+++ src/main/java/com/yj/yjsite/service/UsersService.java
@@ -14,8 +14,7 @@ import com.yj.yjsite.vo.UsersVo;
 
 @Service
 public class UsersService {
-
-	private static final String SAVE_PATH = "";
+	private static final String SAVE_PATH = "/upload";
 	// private static final String URL = "/gallery/assets/";
 
 	@Autowired
